77d1775677c182113f474a7c85ef284634996999
JGScarborough <49846504+JGScarborough@users.noreply.github.com>
2019-09-17 19:59:08 -0400

    Update SortedLinkedList.h

--- SortedLinkedList.h
+++ SortedLinkedList.h
@@ -1,1 +1,20 @@
-//TODO 
+#include "ListNode.h"
+#include "ItemType.h"
+#ifndef SORTEDLINKEDLIST_H
+#define SORTEDLINKEDLIST_H
+class SortedLikedList{
+ public:
+  SortedLinkedList();
+  ~SortedLinkedList();
+  int getLength() const;
+  void insertItem(ItemType item);
+  void deleteItem(ItemType item);
+  int searchItem(ItemType &item);
+  ItemType getNextItem();
+  void resetList();
+ private:
+  int length;
+  ListNode* head;
+  ListNode* currentPos;
+}; // SortedLinkedList                                                                           
+#enddef
