1c043dc1aa8384b62b5edc1b543a4ea379662bda
JGScarborough <49846504+JGScarborough@users.noreply.github.com>
2019-09-17 19:53:00 -0400

    Update ListNode.h

--- ListNode.h
+++ ListNode.h
@@ -1,1 +1,10 @@
+#include "ItemType.h"
+#ifndef LISTNODE_H
+#define LISTNODE_H
 
+class ListNode{
+ public:
+  ItemType item;
+  ListNode* next;
+}; // ListNode                                                                                                                                                                                      
+#endif
