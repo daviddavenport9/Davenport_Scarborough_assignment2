08e2161828175224ad17f1e479f5aa002bff2fe3
daviddavenport9 <45472470+daviddavenport9@users.noreply.github.com>
2019-09-17 20:11:30 -0400

    Update ItemType.h

--- ItemType.h
+++ ItemType.h
@@ -1,1 +1,18 @@
+#ifndef ITEMTYPE_H
+#define ITEMTYPE_H
 
+enum Comparison {LESS, EQUAL, GREATER };
+
+class ItemType{
+
+public : 
+  ItemType();
+  Comparison CompareTo(ItemType) const;
+  void Print() const;
+  void Initialize(int number);
+  
+private:
+  int value;
+
+};
+#endif
