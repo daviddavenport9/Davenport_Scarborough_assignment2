af9685b6c932606a8b8d97e29ec7899d68c8fac2
JGScarborough <49846504+JGScarborough@users.noreply.github.com>
2019-09-18 10:41:55 -0400

    Update SortedLinkedList.cpp

--- SortedLinkedList.cpp
+++ SortedLinkedList.cpp
@@ -1,93 +1,95 @@
 #include "SortedLinkedList.h"
+#include <iostream>
 using namespace std;
 SortedLinkedList::SortedLinkedList(){
     length = 0;
     head = NULL;
-} // SortedLinkedList                                                                                                                                                                               
+} // SortedLinkedList
 SortedLinkedList::~SortedLinkedList(){
   makeEmpty();
-} // destructor                                                                                                                                                                                     
+} // destructor
 void SortedLinkedList::makeEmpty(){
   ListNode *temp;
   while(head != NULL){
     temp = head;
     head = head->next;
     delete temp;
-  } // while                                                                                                                                                                                        
+  } // while
   length = 0;
-} // makeEmpty                                                                                                                                                                                      
-bool SortedLinkedList:isFull() const{
+} // makeEmpty
+bool SortedLinkedList::isFull() const{
   ListNode *loc;
   try{
     loc = new ListNode;
     delete loc;
     return false;
-  }catch{bad_alloc exception){
+  }catch(bad_alloc exception){
     return true;
-  } // try                                                                                                                                                                                          
-} // isFull                                                                                                                                                                                         
-int SortedLinkedList::getLength const(){
+  } // try
+} // isFull
+int SortedLinkedList::getLength() const(){
   return length;
-} // getLength                                         
+} // getLength
 void SortedLinkedList::insertItem(ItemType item){
-  if(isFull()){ // if list is full                                                                                                                                                                  
+  if(isFull()){ // if list is full
     cout << "can't add new item; list is full" << endl;
-  }else{ // if there is space                                                                                                                                                                       
+  }else{ // if there is space
     ListNode *node = new NodeType;
     ListNode *predLoc = NULL;
     ListNode *location = head;
-    node->info = item;
-    bool moreToSearch = (location != NULL); // checks if at end of list                                                                                                                             
+    node->item = item;
+    bool moreToSearch = (location != NULL); // checks if at end of list
     while(moreToSearch){
-      if(location->info.compareTo(item) == LESS){
-        predLoc = location;
-        location = location->next;
-        moreToSearch = (location != NULL);
-      }else{ // nothing else to search                                                                                                                                                              
-        moreToSearch = false;
-      } // if                                                                                                                                                                                       
-    } // while                                                                                                                                                                                      
-    if(predLoc == NULL){ // if list is empty                                                                                                                                                        
+      if(location->item.compareTo(item) == LESS){
+	predLoc = location;
+	location = location->next;
+	moreToSearch = (location != NULL);
+      }else{ // nothing else to search
+	moreToSearch = false;
+      } // if
+    } // while
+    if(predLoc == NULL){ // if list is empty
       node->next = head;
-      head = node; // node becomes head -- first item in list                                                                                                                                       
+      head = node; // node becomes head -- first item in list
     }else{
       node->next = location;
       predLoc->next = node;
-    } // if                                                                                                                                                                                         
+    } // if
     length++;
-  } // if                                                                                                                                                                                           
-} // sortedLinkedList                                                                                                                                                                               
+  } // if
+} // insertLinkedList
 void SortedLinkedList::deleteItem(ItemType item){
-
+  
 } // deleteItem
 int SortedLinkedList::searchItem(ItemType &item){
   int index = 0;
   ListNode *temp = head;
   bool moreToSearch = temp != NULL;
   while(moreToSearch){
-    if(temp->info.compareTo(item) == LESS){ // keep looking                                                                                                                                         
+    if(temp->item.compareTo(item) == LESS){ // keep looking
       temp = temp->next;
       index++;
-    }else if(temp->info.compareTo(item) == EQUAL){ // found                                                                                                                                         
+    }else if(temp->item.compareTo(item) == EQUAL){ // found
       moreToSearch = false;
-    }else{ // at end                                                                                                                                                                                
+    }else{ // at end
       index = -1;
       cout << "Item not found" << endl;
       moreToSearch = false;
-    } // if                                                                                                                                                                                         
-  } // while                                                                                                                                                                                        
+    } // if
+  } // while
   return index;
-} // searchItem                                                                                                                                                                                     
+} // searchItem
 ItemType SortedLinkedList::getNextItem(){
-  if(currentPos == NULL){ // if at end of list OR new list                                                                                                                                          
+  if(currentPos == NULL){ // if at end of list OR new list
     currentPos = head;
-    return currentPos->info;
+    return currentPos->item;
   }else{
     currentPos = currentPos->next;
-    return currentPos->info;
-  } // if                                                                                                                                                                                           
-} // getNextItem                                                                                                                                                                                    
+    return currentPos->item;
+  } // if
+} // getNextItem
 void SortedLinkedList::resetList(){
   currentPos = NULL;
   length = 0;
-} // resetList                         
+} // resetList
+
