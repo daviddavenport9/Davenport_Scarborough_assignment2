779f5a08df9b8b1a882b8ed30729614ab405604b
daviddavenport9 <45472470+daviddavenport9@users.noreply.github.com>
2019-09-17 20:11:57 -0400

    Update ItemType.cpp

--- ItemType.cpp
+++ ItemType.cpp
@@ -1,1 +1,28 @@
+#include "ItemType.h"
+#include <iostream>
 
+using namespace std;
+
+  ItemType::ItemType(){
+    value = 0;
+  }
+
+ Comparison ItemType::CompareTo(ItemType otherItem) const{
+  if (value < otherItem.value){
+    return LESS;
+    }
+  else if (value > otherItem.value){
+    return GREATER;
+    }
+  else{
+    return EQUAL;
+    }
+}
+
+  void ItemType::Print() const{
+  cout << value << endl;
+}
+
+  void ItemType::Initialize(int number){
+   value = number; 
+}
