958af6f3c0a3f1261af5b57f80e8310c6ab2db0f
JGScarborough <49846504+JGScarborough@users.noreply.github.com>
2019-09-17 20:25:28 -0400

    Update SortedLinkedList.cpp

--- SortedLinkedList.cpp
+++ SortedLinkedList.cpp
@@ -1,1 +1,29 @@
-//TODO 
+#include "SortedLinkedList.h"
+int main(){
+  SortedLinkedList::SortedLinkedList(){
+    length = 0;
+    head = NULL;
+  } // SortedLinkedList                                                                          
+  SortedLinkedList::~SortedLinkedList(){
+    resetList();
+  } // destructor                                                                                
+  int SortedLinkedList::getLength const(){
+    return length;
+  } // getLength                                                                                 
+  void SortedLinkedList::insertItem(ItemType item){
+
+  } // sortedLinkedList                                                                          
+  void SortedLinkedList::deleteItem(ItemType item){
+
+  } // deleteItem                                                                                
+  void SortedLinkedList::searchItem(ItemType &item){
+
+  } // searchItem                                                                                
+  ItemType SortedLinkedList::getNextItem(){
+
+  } // getNextItem                                                                               
+  void SortedLinkedList::resetList(){
+    currentPos = NULL;
+    length = 0;
+  } // resetList                                                                                 
+} // main                     
